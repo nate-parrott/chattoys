aba6d8b9153ec66b5ab475b7c3e98c03c208b8b0
Nate Parrott <nate@nateparrott.com>
2025-02-06 22:29:47 -0500

    clean

--- Sources/ChatToys/WebSearch/GoogleSearchEngine.swift
+++ Sources/ChatToys/WebSearch/GoogleSearchEngine.swift
@@ -22,17 +22,17 @@ public struct GoogleSearchEngine: WebSearchEngine {
         request.setValue("utf-8, iso-8859-1;q=0.5", forHTTPHeaderField: "Accept-Charset")
         let userAgent = "Lynx/2.8.8dev.3 libwww-FM/2.14 SSL-MM/1.4.1"
         request.setValue(userAgent, forHTTPHeaderField: "User-Agent")
-        print("[N] Making Google Request '\(query)'")
+//        print("[N] Making Google Request '\(query)'")
         let (data, response) = try await session.data(for: request)
         guard let html = String(data: data, encoding: .isoLatin1) else {
             throw SearchError.invalidHTML
         }
-        print("[BEGIN HTML]")
-        print(html)
-        print("[END HTML]")
+//        print("[BEGIN HTML]")
+//        print(html)
+//        print("[END HTML]")
         let baseURL = response.url ?? urlComponents.url!
 
-        let t2 = CACurrentMediaTime()
+//        let t2 = CACurrentMediaTime()
         let extracted = try extract(html: html, baseURL: baseURL, query: query)
 //        print("[Timing] [GoogleSearch] Parsed at \(CACurrentMediaTime() - t2)")
         
@@ -73,60 +73,6 @@ public struct GoogleSearchEngine: WebSearchEngine {
 }
 
 private extension Fuzi.XMLElement {
-//    func extractYouTubeResults() throws -> [WebSearchResult]? {
-//        var results = [WebSearchResult]()
-//        // Search for elements with a href starting with https://www.youtube.com,
-//        // which contain a div role=heading
-//        // a[href^='https://www.youtube.com']:has(div[role=heading])
-//        for element in css("a[href]") {
-////            guard element.attr("href")?.hasPrefix("https://www.youtube.com") ?? false,
-////                  element.css("div[role='header']").count > 0
-////            else { continue }
-//
-//            if let link = element.attr("href"),
-//               link.hasPrefix("https://www.youtube.com"),
-//               let parsed = URL(string: link),
-//               let title = element.css("div[role='heading'] span").first?.stringValue {
-//                results.append(WebSearchResult(url: parsed, title: title, snippet: nil))
-//            }
-//        }
-//        return results
-//    }
-
-//    func extractSearchResultFromAnchor(baseURL: URL) throws -> WebSearchResult? {
-//        // First, extract the URL
-//        guard let href = attr("href"),
-//              let parsed = URL(string: href, relativeTo: baseURL)
-//        else {
-//            return nil
-//        }
-//        // Then, extract title:
-//        guard let title = css("h3").first?.stringValue.trimmingCharacters(in: .whitespacesAndNewlines) else {
-//            return nil
-//        }
-//
-//        let snippet: String? = { () -> String? in
-//            guard let farParent = self.nthParent(5) else { return nil }
-//            // First, look for an element with `div[style='-webkit-line-clamp:2']`
-//            if let div = farParent.css("div[style='-webkit-line-clamp:2']").first,
-//               let text = div.stringValue.nilIfEmptyOrJustWhitespace {
-//                return text
-//            }
-//
-//            // If not, iterate backwards through child divs (except the first one) and look for one with a non-empty `<span>`
-//            let divChildren = Array(farParent.children.filter { $0.tag == "div" }.dropFirst())
-//            for div in divChildren.reversed() {
-//                if let span = div.css("span").first, let text = span.stringValue.nilIfEmptyOrJustWhitespace {
-//                    return text
-//                }
-//            }
-//            return nil
-//        }()
-//
-
-//        return WebSearchResult(url: parsed, title: title, snippet: snippet)
-//    }
-
     func nthParent(_ n: Int) -> Fuzi.XMLElement? {
         if n <= 0 {
             return self
